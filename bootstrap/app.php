520f33c5de82c797fed3f4c5990cb332a0825ab6
thiagosilva327@live.com <thiago@Jarvis>
2024-09-18 20:34:03 -0300

    fix: fixes-api-routes-according-to-laravel-document

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,8 +6,10 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        api: __DIR__ . '/../routes/api.php',
+        apiPrefix: 'api/',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
-Route::resource('books', BookController::class)->only([
-    'index', 'store', 'show', 'update', 'destroy'
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::resource('book', BookController::class)->only([
+    'index', 'show', 'store', 'update', 'destroy'
 ]);
