941aa01d259e381e8faaeb39dbda8f45936a8bff
thiagosilva327@live.com <thiago@Jarvis>
2024-09-19 10:46:50 -0300

    fix: correct-name-book-api-route-for-books

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
-Route::resource('book', BookController::class)->only([
+Route::resource('books', BookController::class)->only([
     'index', 'show', 'store', 'update', 'destroy'
 ]);
