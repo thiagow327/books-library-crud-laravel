da4ab868badbab9d87326ee3c8bda55cad0084cb
thiagosilva327@live.com <thiago@Jarvis>
2024-09-19 10:20:10 -0300

    refactor: document-methods-in-book-web-controller

--- app/Http/Controllers/BookWebController.php
+++ app/Http/Controllers/BookWebController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BookStoreRequest;
+use App\Http\Requests\BookWebUpdateRequest;
 use App\Models\Book;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class BookWebController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * @return \Illuminate\Contracts\View\View
      */
-    public function index()
+    public function index(): View
     {
         $books = Book::all();
 
@@ -20,16 +23,19 @@ class BookWebController extends Controller
 
     /**
      * Show the form for creating a new resource.
+     * @return \Illuminate\Contracts\View\View
      */
-    public function create()
+    public function create(): View
     {
         return view('books.create');
     }
 
     /**
      * Store a newly created resource in storage.
+     * @param BookStoreRequest $request
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(BookStoreRequest $request): RedirectResponse
     {
         Book::create($request->all());
 
@@ -38,8 +44,10 @@ class BookWebController extends Controller
 
     /**
      * Display the specified resource.
+     * @param string $id
+     * @return \Illuminate\Contracts\View\View
      */
-    public function show(string $id)
+    public function show(string $id): View
     {
         $book = Book::find($id);
 
@@ -48,8 +56,10 @@ class BookWebController extends Controller
 
     /**
      * Show the form for editing the specified resource.
+     * @param string $id
+     * @return \Illuminate\Contracts\View\View
      */
-    public function edit(string $id)
+    public function edit(string $id): View
     {
         $book = Book::find($id);
 
@@ -58,8 +68,11 @@ class BookWebController extends Controller
 
     /**
      * Update the specified resource in storage.
+     * @param BookWebUpdateRequest $request
+     * @param string $id
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, string $id)
+    public function update(BookWebUpdateRequest $request, string $id): RedirectResponse
     {
         $book = Book::find($id);
         $book->update($request->all());
@@ -69,8 +82,10 @@ class BookWebController extends Controller
 
     /**
      * Remove the specified resource from storage.
+     * @param string $id
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function destroy(string $id)
+    public function destroy(string $id): RedirectResponse
     {
         $book = Book::find($id);
         $book->delete();
