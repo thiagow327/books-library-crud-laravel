b5b43e76e9e0c5348b88b4af5888f0bce78568af
thiagosilva327@live.com <thiago@Jarvis>
2024-09-19 08:54:14 -0300

    fix: use-all-methods-to-resource

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookWebController;
 
-Route::resource('book', BookWebController::class)->only([
-    'index',
-    'show',
-    'store',
-    'update',
-    'destroy'
-]);
+Route::resource('books', BookWebController::class);
