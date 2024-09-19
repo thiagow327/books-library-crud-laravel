a88d26599a667e16b87517cb8c28aba508e4f0dd
thiagosilva327@live.com <thiago@Jarvis>
2024-09-18 22:08:41 -0300

    fix: remove-routes-not-used

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
 
 Route::resource('book', BookController::class)->only([
     'index', 'show', 'store', 'update', 'destroy'
